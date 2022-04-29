a077bb6fc532c1ea71e7afe9d31df3e4f3d17409
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-04-29 17:44:46 +0600

    connected to mongo db

---
+++ index.js
@@ -0,0 +1,29 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+require('dotenv').config();
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2x7as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+
+        app.get('/', (req, res) => {
+            res.send('hello Tusher')
+        })
+        app.listen(port, () => {
+            console.log('Listing', port);
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir)
