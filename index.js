8d3d48b283e2a6ab3914c0fc973c99f6fd9a97e4
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-04-30 00:13:51 +0600

    load all data from db by get operation

--- index.js
+++ index.js
@@ -8,15 +8,26 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2x7as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z7fx9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 
 async function run() {
     try {
         await client.connect()
+        const productsCollection = client.db("emaJhonServices").collection('products')
 
-        app.get('/', (req, res) => {
-            res.send('hello Tusher')
+        app.get('/products', async(req, res) => {
+            const query = {}
+            const cursor = productsCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
         })
         app.listen(port, () => {
             console.log('Listing', port);
