70fcdf922dd825c4b1fb8373ad2b1891583d0de3
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-04-30 16:45:38 +0600

    load products conditionally  using query

--- index.js
+++ index.js
@@ -11,31 +11,40 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z7fx9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
 
 
 async function run() {
     try {
         await client.connect()
         const productsCollection = client.db("emaJhonServices").collection('products')
+        
         // Load all data
-        app.get('/products', async(req, res) => {
+        app.get('/products', async (req, res) => {
+            const currentPage = parseInt(req.query.currentPage)
+            const productsOnPage = parseInt(req.query.productsOnPage)
+            
             const query = {}
             const cursor = productsCollection.find(query)
-            const result = await cursor.toArray()
+            
+            //rmv page: currentPage:0 skip (0*productsOnPage) limit (productsOnPage)
+            //rmv page: currentPage:1 skip (1*productsOnPage) limit (productsOnPage)
+            //rmv page: currentPage:2 skip (2*productsOnPage) limit (productsOnPage)
+            
+            let result;
+            if (currentPage || productsOnPage) {
+                 result = await cursor.skip(currentPage*productsOnPage).limit(productsOnPage).toArray()
+                
+            } else {
+                 result = await cursor.toArray()
+                
+            }
             res.send(result)
         })
 
         // load data length
         app.get('/productsLength', async (req, res) => {
-            const query = {}
-            const cursor = productsCollection.find({})
-            // const result = cursor.toArray()
-            const productsLength = await cursor.count()
+            const productsLength = await productsCollection.estimatedDocumentCount()
             res.send({productsLength})
         })
         app.listen(port, () => {
